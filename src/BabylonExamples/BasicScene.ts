4967882df0e0fb30b07b54892e49857cc3b68a71
jgonzosan <jgonzosan@gmail.com>
2021-10-26 16:23:07 -0700

    Basic Scene setup with Babylon and Vue

---
+++ src/BabylonExamples/BasicScene.ts
@@ -0,0 +1,48 @@
+import {
+  Scene,
+  Engine,
+  FreeCamera,
+  Vector3,
+  HemisphericLight,
+  MeshBuilder,
+} from "@babylonjs/core";
+
+export class BasicScene {
+  scene: Scene;
+  engine: Engine;
+
+  constructor(private canvas: HTMLCanvasElement) {
+    this.engine = new Engine(this.canvas, true);
+    this.scene = this.CreateScene();
+
+    this.engine.runRenderLoop(() => {
+      this.scene.render();
+    });
+  }
+
+  CreateScene(): Scene {
+    const scene = new Scene(this.engine);
+    const camera = new FreeCamera("camera", new Vector3(0, 1, -5), this.scene);
+    camera.attachControl();
+
+    const hemiLight = new HemisphericLight(
+      "hemiLight",
+      new Vector3(0, 1, 0),
+      this.scene
+    );
+
+    hemiLight.intensity = 0.5;
+
+    const ground = MeshBuilder.CreateGround(
+      "ground",
+      { width: 10, height: 10 },
+      this.scene
+    );
+
+    const ball = MeshBuilder.CreateSphere("ball", { diameter: 1 }, this.scene);
+
+    ball.position = new Vector3(0, 1, 0);
+
+    return scene;
+  }
+}
---
+++ src/components/BabylonExamples.vue
@@ -0,0 +1,34 @@
+<template>
+
+<div>
+<h3>Babylon Examples</h3>
+<canvas></canvas>
+
+</div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { BasicScene } from '@/BabylonExamples/BasicScene';
+
+export default defineComponent({
+  name: 'BabylonExamples',
+  mounted(){
+    const canvas = document.querySelector("canvas")!;
+    new BasicScene(canvas);
+  }
+  
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+canvas {
+width:70%;
+height:70%;
+
+}
+
+</style>
