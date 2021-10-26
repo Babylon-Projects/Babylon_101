1b1cecbbf884a54dd02aae8ecd3b3966fc8932d4
jgonzosan <jgonzosan@gmail.com>
2021-10-26 16:23:07 -0700

    Basic Scene setup with Babylon and Vue

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+
+  <BabylonExamples />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import BabylonExamples from './components/BabylonExamples.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
+    BabylonExamples
   }
 });
 </script>
