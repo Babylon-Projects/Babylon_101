dbc94494582d65741944026734eaa33e2fdfcd6b
Akong <noobakong@163.com>
2022-06-13 02:12:01 +0800

    docs: 中文注释

--- src/BabylonExamples/StandardMaterials.ts
+++ src/BabylonExamples/StandardMaterials.ts
@@ -26,26 +26,32 @@ export class StandardMaterials {
     const scene = new Scene(this.engine);
     const camera = new FreeCamera("camera", new Vector3(0, 1, -5), this.scene);
     camera.attachControl();
+    // 控制相机的速度 避免出现滚轮太灵敏的情况出现
     camera.speed = 0.25;
 
+    // 创建灯光
     const hemiLight = new HemisphericLight(
       "hemiLight",
       new Vector3(0, 1, 0),
       this.scene
     );
 
+    // 调整灯的亮度
     hemiLight.intensity = 0.75;
-
+    
+    // 创建地面
     const ground = MeshBuilder.CreateGround(
       "ground",
       { width: 10, height: 10 },
       this.scene
     );
 
+    // 创建直径为1的球
     const ball = MeshBuilder.CreateSphere("ball", { diameter: 1 }, this.scene);
-
+    
     ball.position = new Vector3(0, 1, 0);
 
+    // 赋予球和地面材质
     ground.material = this.CreateGroundMaterial();
     ball.material = this.CreateBallMaterial();
 
@@ -53,10 +59,14 @@ export class StandardMaterials {
   }
 
   CreateGroundMaterial(): StandardMaterial {
+    // 创建普通材质
     const groundMat = new StandardMaterial("groundMat", this.scene);
+    
+    // 这里要使用uv对纹理进行缩放调整 但是对应的众多texture都要对应调整后的uv 故此处数组存放 后续遍历调整uv
     const uvScale = 4;
     const texArray: Texture[] = [];
 
+    // 漫反射纹理
     const diffuseTex = new Texture(
       "./textures/stone/stone_diffuse.jpg",
       this.scene
@@ -64,20 +74,25 @@ export class StandardMaterials {
     groundMat.diffuseTexture = diffuseTex;
     texArray.push(diffuseTex);
 
+    // 法线纹理
     const normalTex = new Texture(
       "./textures/stone/stone_normal.jpg",
       this.scene
     );
 
+    // 法线纹理 赋予 凹凸纹理
     groundMat.bumpTexture = normalTex;
+    // 法线贴图有时候会被翻转
     groundMat.invertNormalMapX = true;
     groundMat.invertNormalMapY = true;
     texArray.push(normalTex);
 
+    // 环境光纹理
     const aoTex = new Texture("./textures/stone/stone_ao.jpg", this.scene);
     groundMat.ambientTexture = aoTex;
     texArray.push(aoTex);
 
+    // 镜面纹理  - 反射光
     const specTex = new Texture("./textures/stone/stone_spec.jpg", this.scene);
     groundMat.specularTexture = specTex;
 
@@ -118,6 +133,7 @@ export class StandardMaterials {
 
     const specTex = new Texture("./textures/metal/metal_spec.jpg", this.scene);
     ballMat.specularTexture = specTex;
+    // 镜面效果的程度 光泽度
     ballMat.specularPower = 10;
     texArray.push(specTex);
 
